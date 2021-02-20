9eaccfbd16ad8f71242396bf26b67ac2ab3f57f4
Onur Yaman <onuryaman@gmail.com>
2021-02-21 00:11:18 +0300

    Add obstacles and update grid functionality accordingly

--- path-finding.js
+++ path-finding.js
@@ -2,7 +2,7 @@
 let canvasWidth = 800;
 let canvasHeight = 800;
 let fr = 30;
-let gridSize = 80;
+let gridSize = 20;
 
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
@@ -13,6 +13,7 @@ function setup() {
     .addOperation(function() {
       grid.init().draw();
       target.draw();
+      obstacle.draw();
     }, function() {
       return false;
     })
@@ -53,7 +54,7 @@ var player = {
       self.pos.x += (targetPos.x - startPosition.x) / duration / fr;
       self.pos.y += (targetPos.y - startPosition.y) / duration / fr;
     }
-    
+
     self.draw();
   }
 };
@@ -101,20 +102,20 @@ var manager = {
   },
   runOperations: function() {
     var self = this;
-    
+
     // workaround: make sure that draw is called at least for once.
     if (null === db.get("drawIsRun")) {
       db.set("drawIsRun", true);
       return;
     }
-    
+
     db.get("operations").forEach(operation => function() {
       if (operation.terminate()) {
         self.removeOperation(this);
         return
       }
-        
-        operation.run();
+
+      operation.run();
     }());
 
     return this;
@@ -126,31 +127,89 @@ var manager = {
     }
   }
 };
-  
+
 var grid = {
   data: null,
   init: function() {
-    var cols = Math.round(width/gridSize);
-    var rows = Math.round(height/gridSize);
+    var cols = Math.round(width / gridSize);
+    var rows = Math.round(height / gridSize);
     this.data = new Array(rows);
-    for (var i=0; i<rows; i++) {
-      this.data[i]= new Array(cols);
-      for (var j=0; j<cols; j++) {
-        this.data[i][j] = {};
+    for (var i = 0; i < rows; i++) {
+      this.data[i] = new Array(cols);
+      for (var j = 0; j < cols; j++) {
+        this.data[i][j] = {
+          obstacle: false
+        };
       }
     }
-    
+
     return this;
   },
   draw: function() {
-    for (var i=0; i<this.data.length;i++) {
-      line(0, i*gridSize, width, i*gridSize);
+    for (var i = 0; i < this.data.length; i++) {
+      line(0, i * gridSize, width, i * gridSize);
     }
-    for (i=0; i<this.data[0].length;i++) {
-      line(i*gridSize, 0, i*gridSize, height);
+    for (i = 0; i < this.data[0].length; i++) {
+      line(i * gridSize, 0, i * gridSize, height);
     }
+  },
+  fillCell: function(row, col) {
+    square(row*gridSize, col*gridSize, gridSize);
+  },
+  addObstacle: function(row, col) {
+    var self = this;
+    
+    self.data[row][col].obstacle = true;
+  },
+  removeObstacle: function(row, col) {
+    var self = this;
+    
+    self.data[row][col].obstacle = false;
   }
 };
+
+var obstacle = {
+  data: [],
+  add: function(x, y) {
+    var self = this;
+    var i = Math.floor(x/gridSize);
+    var j = Math.floor(y/gridSize);
+    self.data.push({
+      x: x,
+      y: y,
+      gridRow: i,
+      gridCol: j
+    });
+    
+    grid.addObstacle(i, j);
+  },
+  draw: function() {
+    var self = this;
+    
+    if (keyIsDown(67)) { // 'c'
+      self.data = [];
+      return;
+    }
+    
+    fill(25);
+    self.data.forEach(o => function() {
+      grid.fillCell(o.gridRow, o.gridCol);
+    }());
+    
+    stroke("red");
+    strokeWeight(5);
+    self.data.forEach(o => function() {
+      point(o.x, o.y);
+    }());
+    stroke("black");
+    strokeWeight(1);
+  },
+  
+};
+
+function mouseDragged(e) {
+  obstacle.add(e.x, e.y);
+}
 
 function draw() {
   background(255);
